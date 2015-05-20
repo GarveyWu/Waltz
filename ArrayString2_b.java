40ad439a1869ad3bf7d9b1d92989814c100ee9d9
GarveyWu <van50love@hotmail.com>
2015-05-20 00:42:21 -0700

    cc 1.2_mysol2

---
+++ ArrayString2_b.java
@@ -0,0 +1,14 @@
+package Training;
+
+public class ArrayString2_b {
+	
+	public static void main(String[] args) {
+
+		String s = "invincible";
+		StringBuffer sb = new StringBuffer();
+		sb.append(s);
+
+		System.out.println(sb.reverse().toString());
+
+	}
+}
