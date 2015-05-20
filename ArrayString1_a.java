24b634a493e537ab929d0f2a068442f812a7c0b0
GarveyWu <van50love@hotmail.com>
2015-05-19 23:07:21 -0700

    cc 1.1_sol1

--- ArrayString1_a.java
+++ ArrayString1_a.java
@@ -1,9 +1,13 @@
 package Training;
 
-public class ArrayString1 {
+public class ArrayString1_a {
 
 	public static boolean isUnique(String s) {
 
+		if(s.length() > 256){
+			return false;
+		}
+		
 		boolean[] char_set = new boolean[256];
 		for (int i = 0; i < s.length(); i++) {
 			int val = s.charAt(i);
