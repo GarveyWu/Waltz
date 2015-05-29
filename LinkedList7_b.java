1c371d7f46a13316c0ae858b88683a14016ec806
GarveyWu <van50love@hotmail.com>
2015-05-29 00:12:04 -0700

    cc 2.7_sol2

---
+++ LinkedList7_b.java
@@ -0,0 +1,60 @@
+package Training;
+
+import CtCILibrary.LinkedListNode;
+
+public class LinkedList7_b {
+
+	public static boolean isPalindrome(LinkedListNode head) {
+		
+		int size = 0;
+		LinkedListNode n = head;
+		while (n != null){
+			size++;
+			n = n.next;
+		}
+		Result p = isPalindromeRecurse(head, size);
+		return p.result;
+	}
+	
+	public static Result isPalindromeRecurse (LinkedListNode head, int length){
+		
+		if (head == null || length == 0){
+			return new Result (null, true);
+		} else if (length == 1){
+			return new Result (head.next, true);
+		} else if (length == 2){
+			return new Result (head.next.next, head.data == head.next.data);
+		}
+		Result res = isPalindromeRecurse(head.next, length - 2);
+		if (!res.result || res.node == null){
+			return res;
+		}else{
+			res.result = head.data == res.node.data;
+			res.node = res.node.next;
+			return res;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int length = 10;
+		LinkedListNode[] nodes = new LinkedListNode[length];
+		for (int i = 0; i < length; i++) {
+			nodes[i] = new LinkedListNode(i >= length / 2 ? length - i - 1 : i,
+					null, null);
+		}
+		for (int i = 0; i < length; i++) {
+			if (i < length - 1) {
+				nodes[i].setNext(nodes[i + 1]);
+			}
+			if (i > 0) {
+				nodes[i].setPrevious(nodes[i - 1]);
+			}
+		}
+		LinkedListNode head = nodes[0];
+		System.out.println(head.printForward());
+		nodes[2].data = 3;
+		System.out.println(head.printForward());
+		System.out.println(isPalindrome(head));
+	}
+}
