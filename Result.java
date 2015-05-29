efd1898203d9cdd1fdbd7baa79d903e4f23e648e
GarveyWu <van50love@hotmail.com>
2015-05-29 00:10:30 -0700

    cc 2.7_Class

---
+++ Result.java
@@ -0,0 +1,13 @@
+package Training;
+
+import CtCILibrary.LinkedListNode;
+
+public class Result {
+
+	public LinkedListNode node;
+	public boolean result;
+	public Result (LinkedListNode n, boolean res){
+		node = n;
+		result = res;
+	}
+}
