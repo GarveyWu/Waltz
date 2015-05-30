a8edcf34a039d6aca86a508f38643a4090cedaf1
GarveyWu <van50love@hotmail.com>
2015-05-30 10:02:45 -0700

    cc 3.2_mysol

---
+++ StackandQueue2.java
@@ -0,0 +1,69 @@
+package Training;
+
+import CtCILibrary.AssortedMethods;
+
+public class StackandQueue2 {
+
+	static int stackSize = 20;
+	static int[] buffer = new int[stackSize];
+	static int stackPointer = -1;
+	static int[] minBuffer = new int[stackSize];
+	static int minPointer = -1;
+
+	static void push(int value) throws Exception {
+
+		if (stackPointer + 1 >= stackSize) {
+			throw new Exception("Out of space.");
+		}
+		stackPointer++;
+		if (minPointer == -1) {
+			minPointer++;
+			minBuffer[minPointer] = value;
+		}
+		if (value <= minBuffer[minPointer]) {
+			minPointer++;
+			minBuffer[minPointer] = value;
+		}
+		buffer[stackPointer] = value;
+	}
+
+	static int pop() throws Exception {
+
+		if (stackPointer == -1) {
+			throw new Exception("Trying to pop an empty stack.");
+		}
+		int value = buffer[stackPointer];// get top
+		if (value == minBuffer[minPointer]) {
+			minBuffer[minPointer] = 0;
+			minPointer--;
+		}
+		buffer[stackPointer] = 0;
+		stackPointer--;
+		return value;
+	}
+
+	static int peek() {
+
+		int index = stackPointer;
+		return buffer[index];
+	}
+
+	static int min() {
+
+		return minBuffer[minPointer];
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		for (int i = 0; i < 10; i++) {
+			int value = AssortedMethods.randomIntInRange(0, 100);
+			push(value);
+			System.out.print(value + ", ");
+		}
+		System.out.println("\n");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Popped " + pop());
+			System.out.println("New min is " + min());
+		}
+	}
+}
