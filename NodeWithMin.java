81ec87b0e87d9db7e192d9df8c32f7a917de2661
GarveyWu <van50love@hotmail.com>
2015-05-30 10:02:31 -0700

    cc 3.2_class

---
+++ NodeWithMin.java
@@ -0,0 +1,12 @@
+package Training;
+
+public class NodeWithMin {
+
+	public int value;
+	public int min;
+
+	public NodeWithMin(int value, int min) {
+		this.value = value;
+		this.min = min;
+	}
+}
