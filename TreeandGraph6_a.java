9520046c02626b52fc68de1e4403e57d88059fda
GarveyWu <van50love@hotmail.com>
2015-07-09 19:25:59 -0700

    cc 4.6_sol

---
+++ TreeandGraph6_a.java
@@ -0,0 +1,51 @@
+package Training;
+
+import CtCILibrary.TreeNode;
+
+public class TreeandGraph6_a {
+
+	public static TreeNode inorderSucc(TreeNode n) {
+
+		if (n == null) {
+			return null;
+		}
+		if (n.parent == null || n.right != null) {
+			return leftMostChild(n.right);
+		} else {
+			TreeNode q = n;
+			TreeNode x = q.parent;
+			while (x != null && x.left != q) {
+				q = x;
+				x = x.parent;
+			}
+			return x;
+		}
+	}
+
+	public static TreeNode leftMostChild(TreeNode n) {
+
+		if (n == null) {
+			return null;
+		}
+		while (n.left != null) {
+			n = n.left;
+		}
+		return n;
+	}
+
+	public static void main(String[] args) {
+
+		int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		TreeNode root = TreeNode.createMinimalBST(array);
+		root.print();
+		for (int i = 0; i < array.length; i++) {
+			TreeNode node = root.find(array[i]);
+			TreeNode next = inorderSucc(node);
+			if (next != null) {
+				System.out.println(node.data + "->" + next.data);
+			} else {
+				System.out.println(node.data + "->" + null);
+			}
+		}
+	}
+}
